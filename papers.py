612ca6f341d44546bc0bf2f51fffa27abbe31aaa
goelalex <alex.goel@mail.utoronto.ca>
2014-11-07 11:55:05 -0500

    Changed Spacing on Decide function

--- papers.py
+++ papers.py
@@ -209,8 +209,6 @@ def decide(input_file, watchlist_file, countries_file):
         an entry or transit visa is required, and whether there is currently a medical advisory
     :return: List of strings. Possible values of strings are: "Accept", "Reject", "Secondary", and "Quarantine"
     """
-
-
     #Ask Sasa if returns in this section should be in list or as str; if as str
     if watch_list(input_file, watchlist_file) == ["Secondary"]:
         return ["Secondary"]
